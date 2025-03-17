6b6ba195704c8552c6687fc5c3c4b36295621cb2
CharlotteKim123 <68561316+CharlotteKim123@users.noreply.github.com>
2025-03-16 22:07:43 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function populateStorage() {
+  localStorage.setItem("bgcolor", "red");
+  localStorage.setItem("font", "Helvetica");
+  localStorage.setItem("image", "myCat.png");
+}
+
+
+
+
+https://api.jsonbin.io/v3/b/67d7aba98a456b7966774685
